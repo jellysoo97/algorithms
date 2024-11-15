b2d66c34556e0a3f0465ccc2e072c758720fa93b
jellysoo97 <jellysoo97@gmail.com>
2024-11-16 00:29:06 +0900

    [PGS] Lv.2 / DFS&BFS / 타겟 넘버

--- programmers/level-2/dfs&bfs/타겟 넘버.js
+++ programmers/level-2/dfs&bfs/타겟 넘버.js
@@ -1,19 +1,23 @@
-// dfs
-//   인덱스 = numbers 길이 - 1이면 재귀 종료, 연산 결과값 == target이면 count+1
-//   아니면 다음 인덱스에 대해 dfs 재귀, 연산 업데이트
-function dfs(numbers, target, index, result, count) {
-  if (index === numbers.length - 1) {
-    if (result === target) count++;
-    console.log(index, result, count);
-    return;
+// dfs -> + or -
+//   마지막 depth까지 연산하면(인덱스 === numbers 길이) 재귀 종료
+//     연산 결과값 == target이면 count+1
+//   아니면 다음 depth로(재귀 dfs) -> 인덱스, 연산 결과값 업데이트
+function solution(numbers, target) {
+  let count = 0;
+
+  function dfs(index, result) {
+    if (index === numbers.length) {
+      if (result === target) count++;
+      return;
+    }
+
+    dfs(index + 1, result + numbers[index]);
+    dfs(index + 1, result - numbers[index]);
   }
 
-  dfs(numbers, target, index + 1, result + numbers[index], count);
-  dfs(numbers, target, index + 1, result - numbers[index], count);
+  dfs(0, 0);
+
+  return count;
 }
 
-function solution(numbers, target) {
-  dfs(numbers, target, 0, 0, 0);
-}
-
-solution([4, 1, 2, 1]);
+solution([4, 1, 2, 1], 4);
